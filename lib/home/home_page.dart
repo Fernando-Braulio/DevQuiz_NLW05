23efc8a5e7f588e463ac75fabb460be7a4e52573
Fernando-Braulio <fernandobraulio@outlook.com.br>
2021-04-21 16:00:12 -0300

    Aula 02 - Maximum Speed

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,7 @@
-import 'package:DevQuiz/home/widgets/appbar/app_bar_widget.dart';
+import 'package:DevQuiz/home/widgets/quiz_card/quiz_card_widget.dart';
+
+import 'widgets/appbar/app_bar_widget.dart';
+import 'widgets/level_button/level_button_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,6 +16,36 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                LevelButtonWidget(label: "Fácil"),
+                LevelButtonWidget(label: "Médio"),
+                LevelButtonWidget(label: "Difícil"),
+                LevelButtonWidget(label: "Perito"),
+              ],
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 16,
+                children: [
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
